30e39d237e2fd97d05f2ecf089be8b1effa1f56b
liuzj288 <liuzj288@hotmail.com>
2018-05-15 21:25:17 +0800

    Add files via upload

--- Plugin/Biliplugin.bat
+++ Plugin/Biliplugin.bat
@@ -1,50 +1,51 @@
 @echo OFF
-set version=1.0.1
-title=Bangumiplugin %version%：正在下载 %target%
+set version=1.0.2
+title=Biliplugin %version%：正在下载 %target%
 if "%target%"=="" echo 请使用启动器启动 && pause && exit
 
-:curl_target
-curl -s -k -L -o target_utf8.temp %target%
-iconv -c -f UTF-8 -t GBK  target_utf8.temp > target_gbk.temp
-sed -i "s#\"##g" target_gbk.temp
-sed -i "s#,#\n#g;s#:#\n#g;s#>#>\n#g;s#><#>\n<#g" target_gbk.temp && del *.
-egrep -A1 "^ep_id" target_gbk.temp |egrep "^[0-9]" |sed "s#^#https://www.bilibili.com/bangumi/play/ep#g" |uniq > target_epid.temp
-for /f "delims=" %%p in ('sed -n "$=" target_epid.temp') do set /a epid_num=%%p
-
 :main
-SETLOCAL ENABLEDELAYEDEXPANSION
+echo %target%| findstr /r "[aA][vV]" >nul && echo %target%| sed -r "s#http://www.jijidown.com/video/[aA][vV]##g;s#[aA][vV]##g;s#/##g"> target_av.temp && goto av2cid
+
+:av2cid
+rem 使用唧唧接口获取对应信息
+set /P target_av=<target_av.temp
+curl -s -o target_av2cidutf8.temp http://www.jijidown.com/Api/AvToCid/%target_av%/0
+iconv -c -f UTF-8 -t GBK target_av2cidutf8.temp > target_av2cidgbk.temp
+sed -i "s#:#\n#g;s#,#\n#g;s#/##g;s#}##g;s#哔哩哔哩 (゜-゜)つロ 干杯~#%moviename%##g;s#视频去哪了呢？#%moviename%#g;s#YPE html>##g;s#]##g" target_av2cidgbk.temp
+sed -i "s/\"//g" target_av2cidgbk.temp
+if exist *. del *.
+egrep -A1 "^title" target_av2cidgbk.temp | sed "/title/d;/--/d;/^$/d" | sed -r "s/[[:blank:][:punct:]]/_/g;s/%target_av: =%//g;s/哔哩哔哩弹幕视频网_____゜__゜_つロ__乾杯_____bilibili_______________________________________//g;s/该视频已被B站删除//g" > target_title.temp && set target_title=&& set /P target_title=<target_title.temp
+egrep -A1 "^CID" target_av2cidgbk.temp | sed -r "s/[[:blank:][:punct:]]//g" | sed -r "/^CID/d;/^$/d;/^0$/d" |uniq > target_pcid.temp
+for /f "delims=" %%p in ('sed -n "$=" target_pcid.temp') do set /a pcid_num=%%p
+egrep -A1 "^Title" target_av2cidgbk.temp | sed "/Title/d;/--/d" | sed "s#[[:blank:]][[:blank:]]*#_#g;s#[[:blank:]()>!.]#_#g;s#/##g;s#]##g;s#%moviename%##g" | sed "/^$/d;/哔哩哔哩弹幕视频网/d;/bilibili/d" > target_ptitle.temp && seq -f"P%%02g"  1 1 99 >> target_ptitle.temp
+
+:get_cid
+setlocal enabledelayedexpansion
 set /a m=1001
 set /a n=1
-for /f %%z in (target_epid.temp) do (
+for /f %%z in (target_pcid.temp) do (
+set target_pcid=%%z
+sed -n "!n!p" target_ptitle.temp | sed "s/[mid]/%%t/g" > target_pntitle.temp && set /P target_pntitle=<target_pntitle.temp
+if "!target_pntitle!"=="%target_title%" set target_pntitle=
 cls
-curl -s -k -L -o target_utf8.temp %%z
-iconv -c -f UTF-8 -t GBK  target_utf8.temp > target_gbk.temp
-sed -i "s#\"##g" target_gbk.temp
-sed -i "s#,#\n#g;s#:#\n#g;s#>#>\n#g;s#><#>\n<#g" target_gbk.temp && del *.
-egrep "www.bilibili.com/video/av" target_gbk.temp | sed "s#/#\n#g" |egrep "^av" |sed "s#av##g"> target_av.temp && set /P target_av=<target_av.temp
-egrep "</h1>" target_gbk.temp | sed "s#</h1>##g;s#%moviename%：##g"  > target_ptitle.temp && set /P target_ptitle=<target_ptitle.temp
-egrep -A1 "^cid" target_gbk.temp |egrep "^[0-9]" |head -1 > target_pcid.temp && set /P target_pcid=<target_pcid.temp
-
-echo 正在下载 !n!/%epid_num%：av!target_av!_cid!target_pcid!
+echo 正在下载 !n!/%pcid_num%：av!target_av!_cid!target_pcid!
 call :get_xml !target_pcid!
+
 for %%i in ( %batpath%\temp\cid!target_pcid!.xml )do if %%~zi lss 51200 ( del "%%i" ) else (
-move %batpath%\temp\cid!target_pcid!.xml  "%batpath%\Download\%moviename%（%year%）\av!target_av: =!_P!m:~-2!_%target_title%_!target_ptitle!_cid!target_pcid!.xml" >nul
-call :save_data "!target_pcid: =!" "!target_av: =!" "P!m:~-2! !target_ptitle!" "%target_keyword%"
+move %batpath%\temp\cid!target_pcid!.xml "%batpath%\Download\%moviename%（%year%）\av!target_av: =!_P!m:~-2!_!target_pntitle!_cid!target_pcid!.xml" >nul
+call :save_data "!target_pcid: =!" "!target_av: =!" "P!m:~-2! !target_pntitle!" "!target_title! %target_keyword%"
 )
 set /a n=!n!+1
 set /a m=!m!+1
 )
-SETLOCAL DISABLEDELAYEDEXPANSION
 
 :end
 goto :eof
 
 
-
-::===========================
+::=========================
 ::函数部分开始
-::===========================
-
+::=========================
 :get_xml
 curl -k -L -# --compressed -o "%1_new.xml" http://comment.bilibili.com/%1.xml 
 curl -k -L -s --compressed -o "target_rolldate.temp" http://comment.bilibili.com/rolldate,%1
---
+++ Plugin/danmutools.bat
@@ -0,0 +1,3 @@
+@echo off
+
+if exist "%batpath%\Plugin\danmu-tools\DanMu\*.xml" move "%batpath%\Plugin\danmu-tools\DanMu\*.xml" "%batpath%\Download\%moviename%£¨%year%£©\" 
