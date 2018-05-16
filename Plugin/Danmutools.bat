ad62d44ff293b0b9c138b3ebdbe1625caa901ebf
liuzj288 <liuzj288@hotmail.com>
2018-05-16 14:32:35 +0800

    Update Danmutools.bat

--- Plugin/Danmutools.bat
+++ Plugin/Danmutools.bat
@@ -1,6 +1,8 @@
 @echo off
 cd %batpath%\Plugin\danmu-tools\
+set /p quantity<=quantity.temp
 java -jar %batpath%\Plugin\danmu-tools\downloader.jar -u %target%
-
+set /a quantity=!quantity!+1
+echo %quantity% >quantity.temp
 cd %batpath%\Plugin\danmu-tools\DanMu\ && ren "*.xml" "*." && ren "*." "*_%web%.xml"
-if exist "%batpath%\Plugin\danmu-tools\DanMu\*.xml" move "%batpath%\Plugin\danmu-tools\DanMu\*.xml" "%batpath%\Download\%moviename%£¨%year%£©\" 
+if exist "%batpath%\Plugin\danmu-tools\DanMu\*.xml" move "%batpath%\Plugin\danmu-tools\DanMu\*.xml" "%batpath%\Download\%moviename%ï¼ˆ%year%ï¼‰\" 
