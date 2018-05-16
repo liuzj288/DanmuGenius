0c85d44ea6c0424c1f71c77cf763cf9ffd6f3ad6
liuzj288 <liuzj288@hotmail.com>
2018-05-16 14:20:48 +0800

    Add files via upload

--- Plugin/Danmutools.bat
+++ Plugin/Danmutools.bat
@@ -2,7 +2,5 @@
 cd %batpath%\Plugin\danmu-tools\
 java -jar %batpath%\Plugin\danmu-tools\downloader.jar -u %target%
 
-cd %batpath%\Plugin\danmu-tools\DanMu\
-ren *.xml
-ren *. *_%web%.xml
+cd %batpath%\Plugin\danmu-tools\DanMu\ && ren "*.xml" "*." && ren "*." "*_%web%.xml"
 if exist "%batpath%\Plugin\danmu-tools\DanMu\*.xml" move "%batpath%\Plugin\danmu-tools\DanMu\*.xml" "%batpath%\Download\%moviename%£¨%year%£©\" 
