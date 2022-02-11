8125e5d2547bff8b1d8b0b10bb87f037f1ad551d
liuzj288 <liuzj288@hotmail.com>
2022-02-12 01:17:25 +0800

    Delete install.bat

--- install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set batpath=%~dp0%
-if not exist DanmuGeniusPro.bat echo 警告：请解压至DanmuGeniusPro.bat所在文件夹！！！7 && pause
-if not exist %batpath%Bin\ echo 请下载环境包！！！ && pause
-
-setx PATH "%path%;%batpath%Bin\"
-echo 安装完毕！欢迎使用！
-pause
